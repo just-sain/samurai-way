f52ae0d24a816a6d38723222cae3839508a3d4f5
just-sain <just.sain29@gmail.com>
2022-07-17 21:01:20 +0600

    added github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import store from './redux/redux-store'
@@ -17,9 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const rerenderEntireTree = () => {
 	root.render(
 		<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	)
 }
